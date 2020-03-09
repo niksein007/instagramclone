d310a1617b0f94524dad3a125be48550c22947ba
koser05 <sabhira.koser@gmail.com>
2020-03-09 15:30:25 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './component/navBar.js';
+import camera from './images/camera.png';
+import arrow from './images/arrowicon.png';
+import insta from './images/insta.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <NavBar
+          camera={camera}
+          arrowIcon={arrow}
+          insta={insta} />
+
       </header>
-    </div>
-  );
+
+    )
+  }
 }
 
 export default App;
