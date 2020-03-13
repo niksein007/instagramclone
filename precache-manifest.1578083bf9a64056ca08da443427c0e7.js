8224fcc9c4f155d0ccb38876026cdef3b18ca943
niksein007 <markpearceniksein@gmail.com>
2020-03-13 10:24:38 +0000

    Updates

---
+++ precache-manifest.1578083bf9a64056ca08da443427c0e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beb00579e0aa5c7fff4f92ddb762452",
+    "url": "/github.io/instagramclone/index.html"
+  },
+  {
+    "revision": "d5ba5377b47c72dbd720",
+    "url": "/github.io/instagramclone/static/css/main.ea8834a5.chunk.css"
+  },
+  {
+    "revision": "68f1ebc5d4cd87d40e4b",
+    "url": "/github.io/instagramclone/static/js/2.e1014341.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github.io/instagramclone/static/js/2.e1014341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ba5377b47c72dbd720",
+    "url": "/github.io/instagramclone/static/js/main.84544fb7.chunk.js"
+  },
+  {
+    "revision": "b6f2f92cc80ce71cdd2b",
+    "url": "/github.io/instagramclone/static/js/runtime-main.6894cdc2.js"
+  },
+  {
+    "revision": "c9a61881bea7c7aa1cb287ad1c34c936",
+    "url": "/github.io/instagramclone/static/media/frog.c9a61881.jpg"
+  },
+  {
+    "revision": "20ed3df3b689ba032227989e87397514",
+    "url": "/github.io/instagramclone/static/media/gemma.20ed3df3.jpg"
+  }
+]);
